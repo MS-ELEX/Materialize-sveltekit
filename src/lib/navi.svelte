c417422cab98de0406deec129aa4855ddcc601b0
MS-ELEX <karikomi.ipod@gmail.com>
2025-11-09 17:37:14 +0900

    k

---
+++ src/lib/navi.svelte
@@ -0,0 +1,8 @@
+<template lang="pug">
+  navi
+    div <img src="https://camo.githubusercontent.com/da217d5a848cebbb05d6686e1af0e1b39115870016ac9e4804572d435fad84d6/687474703a2f2f6d6174657269616c697a656373732e636f6d2f7265732f6d6174657269616c697a652e737667"/>
+    div <b>MaterializeCSS</b>
+    ul 
+      li a(href="/about") 概要
+
+</template>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import mcss from '$lib/assets/materialize/css/materialize.min.css';
+	import Navi from '$lib/navi.svelte';
+	//l	import Navi2 from '$lib/navi.md';
+	let a = 'tyo';
 
 	let { children } = $props();
 </script>
@@ -8,4 +12,22 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<template lang="pug">
+	Navi 
+	// Navi2
+</template>
+{a}
+
 {@render children()}
+
+<style lang="scss">
+	// $color: #00ff00;
+	$color: #008800;
+
+	h1 {
+		color: $color;
+	}
+	.cls {
+		color: $color;
+	}
+</style>
