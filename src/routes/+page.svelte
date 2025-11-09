74cc67a302eb6b5939ff687db797193be525838b
MS-ELEX <karikomi.ipod@gmail.com>
2025-11-09 15:25:21 +0900

    sass pug　対応

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<template lang="pug">
+    h1 Welcome
+    div kl;
+</template>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,13 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex()],
+	preprocess: [sveltePreprocess(), vitePreprocess(), mdsvex()],
 	kit: { adapter: adapter() },
 	extensions: ['.svelte', '.svx']
 };
